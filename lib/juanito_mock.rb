b411f562dccefae10c45ebd2902f6cbe377862fc
Juanito Fatas <katehuang0320@gmail.com>
2015-08-19 21:30:02 +0800

    First implementation of reset functionality

--- lib/juanito_mock.rb
+++ lib/juanito_mock.rb
@@ -7,18 +7,42 @@ module JuanitoMock
     end
 
     def to(definition)
-      Stubber.new(@obj).stub(definition)
+      Stubber.for_object(@obj).stub(definition)
     end
   end
 
   class Stubber
+    def self.stubbers
+      @stubbers ||= {}
+    end
+
+    def self.for_object(obj)
+      stubbers[obj.__id__] ||= Stubber.new(obj)
+    end
+
+    def self.reset
+      stubbers.each_value(&:reset)
+      stubbers.clear
+    end
+
     def initialize(obj)
       @obj = obj
+      @definitions = []
     end
 
     def stub(definition)
+      @definitions << definition
+
       @obj.define_singleton_method definition.message do
         definition.return_value
+      end
+    end
+
+    def reset
+      @definitions.each do |definition|
+        @obj.singleton_class.class_eval do
+          remove_method(definition.message) if method_defined?(definition.message)
+        end
       end
     end
   end
@@ -47,6 +71,7 @@ module JuanitoMock
   end
 
   def self.reset
+    Stubber.reset
   end
 end
 
