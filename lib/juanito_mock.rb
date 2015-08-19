c50120189bcc3f2fe051bbc8ea8513b9a4979582
Juanito Fatas <katehuang0320@gmail.com>
2015-08-19 21:08:18 +0800

    First passing test! Yay!

--- lib/juanito_mock.rb
+++ lib/juanito_mock.rb
@@ -1,5 +1,42 @@
 require "juanito_mock/version"
 
 module JuanitoMock
-  # Your code goes here...
+  class StubTarget
+    def initialize(obj)
+      @obj = obj
+    end
+
+    def to(definition)
+      @obj.define_singleton_method definition.message do
+        definition.return_value
+      end
+    end
+  end
+
+  class ExpectationDefinition
+    attr_reader :message, :return_value
+
+    def initialize(message)
+      @message = message
+    end
+    
+    def and_return(return_value)
+      @return_value = return_value
+      self
+    end
+  end
+
+  module TestExtensions
+    def allow(obj)
+      StubTarget.new(obj)
+    end
+
+    def receive(message)
+      ExpectationDefinition.new(message)
+    end
+  end
+end
+
+class Minitest::Test
+  include JuanitoMock::TestExtensions
 end
--- test/juanito_mock_test.rb
+++ test/juanito_mock_test.rb
@@ -1,5 +1,11 @@
 require "test_helper"
 
 describe JuanitoMock do
+  it "allows an object to receive a message and returns a value" do
+    warehouse = Object.new
 
+    allow(warehouse).to receive(:full?).and_return(true)
+
+    warehouse.full?.must_equal true
+  end
 end
